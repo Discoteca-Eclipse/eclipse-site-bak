d53f033e634a5b1827b3e7b7e9364c5efeeda958
Anthony Leon <anthonyfabian22lldev@gmail.com>
2025-04-02 07:41:24 +0200

    d

--- src/components/react/HomeDjsSwiper/HomeDjsSwiper.tsx
+++ src/components/react/HomeDjsSwiper/HomeDjsSwiper.tsx
@@ -1,16 +1,18 @@
 import 'swiper/scss'
-import 'swiper/scss/effect-coverflow'
+import 'swiper/scss/effect-cards'
 import 'swiper/scss/pagination'
 import './HomeDjsSwiper.scss'
 
 import {Swiper, SwiperSlide} from 'swiper/react';
-import {EffectCoverflow, Pagination} from 'swiper/modules';
+import {EffectCards} from 'swiper/modules';
 import {useEffect, useState} from "react";
 import {getDjs} from "../../../services/djs.service.ts";
 import type {EntityToList} from "../../../models";
+import {getTranslation} from "../../../utils/i18n.ts";
 
 export const HomeDjsSwiper = () => {
   const [djs, setDjs] = useState<EntityToList[] | null>(null)
+  const lang = window.location.pathname.split('/')[1] ?? 'en';
 
   useEffect(() => {
     getDjs().then(value => setDjs(value))
@@ -22,25 +24,16 @@ export const HomeDjsSwiper = () => {
 
   return (
     <section className="djs-swiper-container">
+      <h1>{getTranslation(lang, 'home.djs')}</h1>
+
       <Swiper
-        effect={'coverflow'}
+        effect={'cards'}
         grabCursor={true}
-        centeredSlides={true}
-        slidesPerView={'auto'}
-        loop={true}
-        coverflowEffect={{
-          rotate: 50,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: true,
-        }}
-        pagination={true}
-        modules={[EffectCoverflow, Pagination]}
+        modules={[EffectCards]}
         className="mySwiper"
       >
         {djs.map(value =>
-          <SwiperSlide>
+          <SwiperSlide key={value.id}>
             <img src={value.imageUrl} alt={value.name}/>
           </SwiperSlide>
         )}
